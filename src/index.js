55d65fd49ad4028df75981f6fb2805116c7b8968
ameraashraf <ameraashraf9871@gmail.com>
2023-09-13 18:00:05 +0300

    added bootstrap.bundle.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/react-fontawesome";
 import "./index.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
